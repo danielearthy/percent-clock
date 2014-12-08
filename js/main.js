62c33ec0d08225d17010a20e41c2837fde8923f6
Daniel Earthy <danielearthy@gmail.com>
2014-12-08 09:00:42 -0500

    capitalization changes

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ var MinutesInDay = 1440;
 var clock = {
   currentTime: null,
 
-    updateTime : function(){
+  updateTime : function(){
     var _date = new Date();
     var _hoursInMinutes = _date.getHours() * 60;
     var _minutesInHour = _date.getMinutes();
-    var _SecondsInMinutes = _date.getSeconds() / 60;
-    var _totalMinutes = _hoursInMinutes + _minutesInHour + _SecondsInMinutes;
+    var _secondsInMinutes = _date.getSeconds() / 60;
+    var _totalMinutes = _hoursInMinutes + _minutesInHour + _secondsInMinutes;
     var _percentTime = (_totalMinutes / MinutesInDay *  100).toFixed(3);
     this.currentTime = _percentTime
   },
